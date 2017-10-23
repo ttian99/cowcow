b58af8699e3379b37b20b8d849f9f111ac49b2f0
ttian99 <176553611@qq.com>
2017-10-23 21:01:04 +0800

    添加room_list

---
+++ assets/script/audio-mgr.js
@@ -0,0 +1,27 @@
+
+class AudioMgr {
+  constructor() {
+    console.log('init audioMgr');
+  }
+
+  playMusic() {
+
+  }
+
+  playEffect() {
+
+  }
+
+  stopMusic() {
+
+  }
+
+  stopEffect() {
+
+  }
+}
+
+
+const audioMgr = new AudioMgr();
+
+export default audioMgr;
